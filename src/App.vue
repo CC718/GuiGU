320a66c4e045bd05512206338d1e1f2df2703110
CC718 <CC86718@163.com>
2024-05-27 01:53:58 +0800

    svg配置+测试+svg文件导入

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
    <div>
-      <el-button type="primary">Primary</el-button>
+      <svg-icon name="home" color="red"></svg-icon>
    </div>
 </template>
 
 <script setup lang="ts">
-
+import SvgIcon from '@/components/SvgIcon/index.vue'
 </script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import 'element-plus/dist/index.css'
 //@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import App from './App.vue'
+//svg需要配置代码
+import 'virtual:svg-icons-register'
 
 const app = createApp(App)
 
